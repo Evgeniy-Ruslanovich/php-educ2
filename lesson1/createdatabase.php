820ee01480bf402fe9d113d118b2e686544fcf3d
Evgeniy.Ruslanovich <evgeniy.zaynutdinov@gmail.com>
2016-09-17 15:26:31 +0400

    Создал файл, в котором будет создаваться база данных

--- lesson1/createdatabase.php
+++ lesson1/createdatabase.php
@@ -1,1 +1,14 @@
-﻿
+﻿<?php
+
+//соединяемся с сервером базы данных
+
+$link = mysqli_connect('localhost', 'root', '');
+if (!$link) {
+    die('Ошибка подключения ' . mysqli_connect_errno() . ') '
+       . mysqli_connect_error());
+}
+echo 'Успешно соединились ' . mysqli_get_host_info($link) . "\n";
+
+//создаем базу данных
+
+mysqli_query($link, '', MYSQLI_STORE_RESULT);
